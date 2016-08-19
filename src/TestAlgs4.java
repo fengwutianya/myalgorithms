ddb16f0612d08cc81800497e806f6efd2525fed8
fengwutianya <949979029@qq.com>
2016-08-19 20:08:49 +0800

    must do

---
+++ src/TestAlgs4.java
@@ -0,0 +1,10 @@
+/**
+ * Created by xuan on 2016/8/19.
+ */
+import edu.princeton.cs.algs4.StdIn;
+public class TestAlgs4 {
+    public static void main(String[] args) {
+        int i = StdIn.readInt();
+        System.out.println(i);
+    }
+}
