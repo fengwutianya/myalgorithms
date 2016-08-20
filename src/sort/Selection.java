53f53828caa8b9e351fecfcd469e9cbf77e829d8
fengwutianya <949979029@qq.com>
2016-08-20 14:42:24 +0800

    Selection 选择排序 每一次循环保证最开始元素最小

--- src/sort/Selection.java
+++ src/sort/Selection.java
@@ -4,15 +4,19 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
+ * 1.运行时间与输入无关 N-1 + N-2... + 2 + 1(扫描） + N（移动）
+ * 2.修改之后，数据移动次数最少N，自己还是年轻啊= =。
+ *      先找出最小坐标在移动比 每次都移动 要好
  * Created by xuan on 2016/8/20.
  */
 public class Selection {
     public static void sort(Comparable[] a) {
         for (int i = 0; i < a.length; i++) {
+            int min = i;
             for (int j = i+1; j < a.length; j++) {
-                if (less(a[j], a[i]))
-                    exch(a, i, j);
+                if (less(a[j], a[min])) min = j;
             }
+            exch(a, i, min);
         }
     }
 
