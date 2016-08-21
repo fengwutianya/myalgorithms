28ae5f9bc2b0ef296c6c51821e8ca4620ebaded7
fengwutianya <949979029@qq.com>
2016-08-21 23:20:28 +0800

    不同算法排序时间对比

--- src/sort/SortCompare.java
+++ src/sort/SortCompare.java
@@ -5,17 +5,19 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
+ * 1.对象的相等用equals，比方此处的String。
  * Created by xuan on 2016/8/20.
  */
 public class SortCompare {
     public static double time(String alg, Comparable[] a) {
         Stopwatch timer = new Stopwatch();
-        if (alg == "Insertion") Insertion.sort(a);
-        if (alg == "Selection") Selection.sort(a);
-        if (alg == "Shell") Shell.sort(a);
-        if (alg == "Merge") Merge.sort(a);
-        if (alg == "Quick") Quick.sort(a);
-        if (alg == "Heap") Heap.sort(a);
+        if (alg.equals("Insertion")) Insertion.sort(a); //此处用equals，用==有错
+        if (alg.equals("Selection")) Selection.sort(a);
+        /*if (alg.equals("Shell")) Shell.sort(a);
+        if (alg.equals("Merge") Merge.sort(a);
+        if (alg.equals("Quick")) Quick.sort(a);
+        if (alg.equals("Heap")) Heap.sort(a);
+        */
         return timer.elapsedTime();
     }
 
@@ -27,7 +29,7 @@ public class SortCompare {
         Double[] a = new Double[N];
         for (int i = 0; i < T; i++) {
             for (int j = 0; j < N; j++) {
-                a[i] = StdRandom.uniform();
+                a[j] = StdRandom.uniform();
             }
             total += time(alg, a);
         }
@@ -41,6 +43,7 @@ public class SortCompare {
         int T = Integer.parseInt(args[3]);
         double t1 = timeRandomInput(alg1, N, T);
         double t2 = timeRandomInput(alg2, N, T);
+        //System.out.println(t1 + " " + t2);
         StdOut.printf("For %d random Doubles%n  %s is", N, alg1);
         StdOut.printf(" %.1f times faster than %s\n", t2/t1, alg2);
     }
