bbad3e38e42c3468da86159580472354c08d9a6c
fengwutianya <949979029@qq.com>
2016-08-22 10:05:07 +0800

    Shell排序 我还不懂为什么他比插入排序要快 改进就是 插入排序每次相邻交换 然而希尔排序是相邻h交换，最终h=1，排序完成，三重循环。

--- src/sort/SortCompare.java
+++ src/sort/SortCompare.java
@@ -13,8 +13,8 @@ public class SortCompare {
         Stopwatch timer = new Stopwatch();
         if (alg.equals("Insertion")) Insertion.sort(a); //此处用equals，用==有错
         if (alg.equals("Selection")) Selection.sort(a);
-        /*if (alg.equals("Shell")) Shell.sort(a);
-        if (alg.equals("Merge") Merge.sort(a);
+        if (alg.equals("Shell")) Shell.sort(a);
+        /*if (alg.equals("Merge") Merge.sort(a);
         if (alg.equals("Quick")) Quick.sort(a);
         if (alg.equals("Heap")) Heap.sort(a);
         */
